94ac3354ff30032140d0b41728b0879c80686f72
wuyifan678 <469234777@qq.com>
2020-04-15 17:56:56 +0800

    测试git备注2020-4-12

--- testprovider/src/main/java/com/wuyifan/testprovider/controller/EmpController.java
+++ testprovider/src/main/java/com/wuyifan/testprovider/controller/EmpController.java
@@ -2,29 +2,44 @@ package com.wuyifan.testprovider.controller;
 
 import com.wuyifan.testprovider.bean.EmpBean;
 import com.wuyifan.testprovider.service.Empservice;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@Slf4j
+@Api("这是测试员工emp表的controller")
 public class EmpController {
     @Autowired
     Empservice empservice;
 
     @RequestMapping("query")
+    @ApiOperation(value = "这是全查员工表的路径")
     public List<EmpBean> query() {
+        // 记录debug级别的信息
+        log.debug("This is debug message.");
+        // 记录info级别的信息
+        log.info("This is info message.");
+        // 记录error级别的信息
+        log.error("This is error message.");
+
+
         return empservice.queryEmp();
     }
 
     @RequestMapping("download")
+    @ApiOperation("这是把员工表输出到D盘的路径")
     public int download() throws IOException {
         //查出emp表的数据
         List<EmpBean> empBeans = empservice.queryEmp();
---
+++ testprovider/src/main/java/com/wuyifan/testprovider/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.wuyifan.testprovider.controller;
+
+/**
+ *
+ *
+ * @Author:wuyifan
+ * @Describe: 描述
+ * @Date:2020-04-14
+ * @Time:17:31
+ */
+public class TestController {
+   /**
+   *description 
+    * param []
+    * return void
+    * author wuyifan
+    * createTime 2020-04-14 17:40
+    **/
+    public void Test(){
+
+    }
+}
